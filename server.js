0a7f4742ed9111f538c6ea45002f40270c327270
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-25 17:19:47 +0100

    Set up endpoints in server for each view file

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.render("index");
 });
+
+app.get('/users/register', (req, res) => {
+    res.render("register");
+})
+
+app.get('/users/login', (req, res) => {
+    res.render("login");
+})
+
+app.get('/users/dashboard', (req, res) => {
+    res.render("dashboard", { user: "Sophie" });
+})
 
 app.listen(PORT, () => {
     console.log("Server running on port" + PORT)
