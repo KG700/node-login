fc53ba50d7cca918e6452534e8de093e88b045cc
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-26 11:49:37 +0100

    Added validation to register form

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { pool } from './dbConfig';
+import { pool } from './dbConfig.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render("index");
@@ -21,6 +22,34 @@ app.get('/users/login', (req, res) => {
 
 app.get('/users/dashboard', (req, res) => {
     res.render("dashboard", { user: "Sophie" });
+})
+
+app.post('/users/register', (req, res) => {
+    let { name, email, password, password2 } = req.body;
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    })
+
+    let errors = []
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ message: 'Please enter all fields' })
+    }
+
+    if (password.length < 6) {
+        errors.push({ message: 'Password should be at least 6 characters' })
+    }
+
+    if (password !== password2) {
+        errors.push({ message: 'Passwords do not match' })
+    }
+
+    if (errors.length > 0) {
+        res.render('register', { errors })
+    }
 })
 
 app.listen(PORT, () => {
